8a0166e264e0d4541ab0f843688ce429dfa64476
Abhishekgoyal007 <abhishekgoyal1311@gmail.com>
2025-11-03 20:12:17 +0530

    Hero Section Completed

---
+++ src/components/HeroText.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { FlipWords } from './FlipWords'
+import { motion } from 'motion/react'
+
+const HeroText = () => {
+    const words = ['Innovative', 'User Friendly', 'High Quality'];
+    const variants = {
+        hidden: {opacity: 0, x: -50},
+        visible: {opacity: 1, x: 0}
+    }
+    return (
+    <div 
+        className='z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text'
+    >
+        {/* Desktop view */}
+        <div className='flex-col hidden md:flex c-space'>
+            <motion.h1 variants={variants} initial="hidden" animate="visible" transition={{delay: 1}} className='text-4xl font-medium'>Hi there, I'm Abhishek</motion.h1>
+            <div className='flex flex-col items-start'>
+                <motion.p variants={variants} initial="hidden" animate="visible" transition={{delay: 1.2}} className='text-5xl font-medium text-neutral-300'>A Developer Dedicated to Crafting</motion.p>
+                <motion.div
+                    variants={variants} initial="hidden" animate="visible" transition={{delay: 1.5}}>
+                    <FlipWords words={words} className='text-8xl font-black text-white' />
+                </motion.div>
+                <motion.p variants={variants} initial="hidden" animate="visible" transition={{delay: 1.8}} className='text-4xl font-medium text-neutral-300'>Web Solutions</motion.p>
+            </div>
+        </div>
+        {/* Mobile view */}
+        <div className='flex flex-col space-y-6 md:hidden'>
+            <motion.p variants={variants} initial="hidden" animate="visible" transition={{delay: 1}} className='text-4xl font-medium'>Hi there, I'm Abhishek</motion.p>
+            <div>
+                <motion.p variants={variants} initial="hidden" animate="visible" transition={{delay: 1.2}} className='text-5xl font-black text-neutral-300'>Building</motion.p>
+                <motion.div
+                    variants={variants} initial="hidden" animate="visible" transition={{delay: 1.5}}>
+                    <FlipWords words={words} className='text-7xl font-bold text-white' />
+                </motion.div>
+                <motion.p variants={variants} initial="hidden" animate="visible" transition={{delay: 1.8}} className='text-4xl font-black text-neutral-300'>Web Applications</motion.p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroText
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,37 @@
+import { Canvas, useFrame } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import HeroText from '../components/HeroText'
+import ParallaxBackground from '../components/parallaxBackground'
+import { Astronaut } from '../components/Astronaut'
+import { Float, OrbitControls } from '@react-three/drei'
+import { useMediaQuery } from 'react-responsive'
+import { easing } from 'maath'
+import Loader from '../components/Loader'
+
+
+const Hero = () => {
+  const isMobile = useMediaQuery({maxWidth: 835})
+  return (
+    <section 
+        className='flex item-start justify-center md:items-start md:justify-start overflow-hidden min-h-screen c-space'>
+        <HeroText />
+        <ParallaxBackground />
+        <figure className='top-20 absolute inset-0' style={{width:"100vw", height: "100vh"}}>
+            <Canvas camera={{position: [0, 1, 3]}}>
+                <Suspense fallback={<Loader />}><Float><Astronaut scale={isMobile && 0.20} position={isMobile && [0, -1.5, 0]}/></Float></Suspense>
+                {/* <OrbitControls /> */}
+                <Rig /> 
+            </Canvas>
+        </figure>
+    </section>
+  )
+}
+
+function Rig(){
+    useFrame((state, delta)=>{
+        easing.damp3(state.camera.position, [state.mouse.x / 10, 1+state.mouse.y/10, 3], 0.5, delta)
+    })
+    return null
+}
+
+export default Hero
